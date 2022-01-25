dc0503c190582a1c90e530877f26c295eda13929
Anurag Singh <83186717+anuragsingh6886@users.noreply.github.com>
2022-01-25 09:45:53 +0530

    Update and rename script1.js to script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
 const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
